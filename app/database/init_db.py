246647418a4cb75b757e459d77045c6488cde7f3
LimaXII_ <luccasdasilvalima@hotmail.com>
2026-02-21 21:57:49 -0300

    fix: fixed database message

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -3,7 +3,7 @@ import app.models
 
 def init_db():
     Base.metadata.create_all(bind=engine)
-    print("Banco inicializado com sucesso!")
+    print("The database has been successfully created.")
 
 if __name__ == "__main__":
     init_db()
